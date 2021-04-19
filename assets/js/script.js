3e07adeb506f5237803570334f1e03fda2180a27
Brady Taylor <baetay03@gmail.com>
2021-04-18 20:12:22 -0600

    start of time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
+//Moment decarations for the day and time of the schedular
 moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 // Moment Current time 
-var currentTime = moment();
-// Returns current time to the nearest hour - 12:30 becomes 12:00
-currentTime = currentTime.startOf("hour");
-// Calculates the start of day + 9 to return 9 am
+var theCurrentTime = moment();
+
+theCurrentTime = theCurrentTime.startOf("hour");
+
 var beforeTime = moment().startOf('day').add(9, "hours");
+
+//This section is where I will put my time blocks.
+
+//My 9AM time block is here
+var time9Am = beforeTime.add(0, "h");
+time9Am = time9Am.format('hh:mm A');
+$(".timeBlock1").text(time9Am);
+
+//My 10AM time block is here
+var time10Am = beforeTime.add(1, "h");
+time10Am = time10Am.format('hh:mm A');
+$(".timeBlock2").text(time10Am);
+
+//My  11Am time block is here
+var time11Am = beforeTime.add(1, "h");
+time11Am = time11Am.format('hh:mm A');
+$(".timeBlock3").text(time11Am);
+
+//My 12PM time block is here
